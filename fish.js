1221c530ffdc1117a08b91a901ab5d04db901830
BlownCapacitor <52745475+BlownCapacitor@users.noreply.github.com>
2022-03-29 11:24:19 +0530

    Add files via upload

---
+++ fish.js
@@ -0,0 +1,36 @@
+AFRAME.registerComponent("fish", {
+    init: function () {
+      for (var i = 1; i <= 10; i++) {
+        const id = `fish${i}`;
+  
+        const posX = Math.random() * 100 + -50;
+        const posY = Math.random() * 5 + 5;
+        const posZ = Math.random() * 60 + -40;
+        const position = { x: posX, y: posY, z: posZ };
+        this.Fish(id, position);
+      }
+    },
+    Fish: function (id, position) {
+      const fishEntity = document.querySelector("#terra");
+      var fishEl = document.createElement("a-entity");
+  
+      fishEl.setAttribute("id", id);
+      fishEl.setAttribute("position", position);
+      fishEl.setAttribute("rotation", { x: 0, y: 180, z: 0 });
+  
+      fishEl.setAttribute("scale", { x: 0.3, y: 0.3, z: 0.3 });
+  
+      fishEl.setAttribute("gltf-model", "fish/scene.gltf");
+  
+      
+      fishEl.setAttribute("animation", {
+        property: "position",
+        to: "100 10 -20",
+        loop: "true",
+        dur: 20000
+      });
+  
+        fishEntity.appendChild(fishEl);
+    }
+  })
+  
